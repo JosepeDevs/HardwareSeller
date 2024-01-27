35f77e21631202f41b19f00c7da70ca53d864740
Josepe <josepedevs@gmail.com>
2024-01-27 02:11:45 +0100

    falta validacióon de articulos para terminar de refactorizar todas las vistas refalta revisar controladores y terminar de sacar archivos de models que no van ahí o serán una una función de aalgún model3

--- Models/Articulo.php
+++ Models/Articulo.php
@@ -1,13 +1,13 @@
 <?php
 if(session_status() !== PHP_SESSION_ACTIVE) {session_start();}
-include_once("UserSession.php");
+include_once("../Controllers/UserSession.php");
 $usuarioLogeado = UserEstablecido();
 if( $usuarioLogeado == false){
     session_destroy();
     echo "Articulo dice: no está user en session";
     header("Location: index.php");
 }
-include_once("conectarBD.php");
+include_once("../config/conectarBD.php");
 
 class Articulo {
 
