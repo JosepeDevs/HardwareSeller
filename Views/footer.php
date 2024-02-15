7cbb60b0c2775814b2c101b489a1744e341c47d3
josepedevs <josepedevs@gmail.com>
2024-02-15 21:08:23 +0100

    añadimos badge para probar github actions autodeploy

--- Views/footer.php
+++ Views/footer.php
@@ -21,6 +21,8 @@
         <a href="mailto:info@example.com">Escribenos un correo</a>
         <a href="https://www.example.com/contact">Contacto web</a>
     </div>
+    <div>[<img alt="Deployed with FTP Deploy Action" src="https://img.shields.io/badge/Deployed With-FTP DEPLOY ACTION-%3CCOLOR%3E?style=for-the-badge&color=0077b6">](https://github.com/SamKirkland/FTP-Deploy-Action)
+</div>
 </footer>
 </body>
 </html>
