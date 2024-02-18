7714f18eb98a5c806776794eb68ffdee428ce64d
josepedevs <josepedevs@gmail.com>
2024-02-18 20:31:22 +0100

    aside dentro de div

--- Views/Catalogo.php
+++ Views/Catalogo.php
@@ -3,13 +3,15 @@ include("header.php");
 ?>
 
 <h1>Catálogo</h1>
-<aside class="col-lg-3 col-md-1 col-12">
-    <a href="?prebuilt">Pre-built computers</a>
-    <a href="?Pantallas">Pantallas</a>
-    <a href="?Graficas">Gráficas</a>
-    <a href="?Mobo">Placas base</a>
-    <a href="?RAM">RAM</a>
-</aside>
+<div class="aside">
+    <aside class="col-lg-3 col-md-1 col-12">
+        <a href="?prebuilt">Pre-built computers</a>
+        <a href="?Pantallas">Pantallas</a>
+        <a href="?Graficas">Gráficas</a>
+        <a href="?Mobo">Placas base</a>
+        <a href="?RAM">RAM</a>
+    </aside>
+<div>
 <div class="col-lg-9 col-md-11 col-12">
     <table>
         <tr>
