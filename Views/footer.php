10a587c9bbf2175dd4177ff7347ca131609d7e7d
josepedevs <josepedevs@gmail.com>
2024-02-18 18:34:10 +0100

    footer recortadisimo

--- Views/footer.php
+++ Views/footer.php
@@ -1,6 +1,7 @@
 </div><!--esto cierra "cta-container"-->
 </div><!--esto cierra "<div class="cuerpo"-->
 <footer class="footer">
+<div class="col-lg-3 col-md-6 col-12">    
     <div>
         <a href="/index.php"><img src="/Resources/HSLogoFondoAzulGrisSinTexto.png" width="100px" alt="Logo"></a>
     </div>
@@ -13,31 +14,17 @@
         <h3>Contacto</h3>
         <a href="mailto:info@josepedevs.infinityfreeapp.com">Escribenos un correo</a>
     </div>
-    <div class="col-lg-3 col-md-6 col-12">
-                                <!-- Single Widget -->
-                                <div class="single-footer sm-custom-border f-link">
-                                    <h3>Legal</h3>
-                                    <ul>
-                                        <li><a href="/Views/cookies.php">Cookies</a></li>
-                                        <li><a href="/Views/Privacidad.php">Privacidad</a></li>
-                                        <li><a href="/Views/AvisoLegal.php">Aviso legal</a></li>
-                                    </ul>
-                                </div>
-                            </div>
-    <div>
-        <img alt="Deployed with FTP Deploy Action" src="https://img.shields.io/badge/Deployed With-FTP DEPLOY ACTION-%3CCOLOR%3E?style=for-the-badge&color=0077b6">
-        <a href=https://github.com/SamKirkland/FTP-Deploy-Action>Házlo tú mismo. Credit @SamKirkland</a>
-    </div>
-    <!-- Start Footer Area -->
-        <div class="footer-middle">
-            <div class="container">
-                <div class="row">
-                    <div class="col-lg-4 col-md-4 col-12">
-                    </div>
-                    <div class="col-lg-8 col-md-8 col-12">
-                        <div class="row">
-
-                                <div class="footer-social">
+    
+        <!-- Single Widget -->
+        <div class="single-footer sm-custom-border f-link">
+            <h3>Legal</h3>
+            <ul>
+                <li><a href="/Views/cookies.php">Cookies</a></li>
+                <li><a href="/Views/Privacidad.php">Privacidad</a></li>
+                <li><a href="/Views/AvisoLegal.php">Aviso legal</a></li>
+            </ul>
+        </div>
+        <div class="footer-social">
                                     <ul>
                                         <li><a href="#"><i class="lni lni-facebook-original"></i></a></li>
                                         <li><a href="#"><i class="lni lni-twitter-original"></i></a></li>
@@ -45,30 +32,11 @@
                                         <li><a href="#"><i class="lni lni-google"></i></a></li>
                                     </ul>
                                 </div>
-                                <!-- End Single Widget -->
-                            </div>
-                            <div class="col-lg-3 col-md-6 col-12">
-                            </div>
-                            <div class="col-lg-3 col-md-6 col-12">
-                                <!-- Single Widget -->
-                                <div class="single-footer md-custom-border sm-custom-border f-contact">
-                                    <h3>Contacto</h3>
-                                    <ul>
-                                        <li><i class="lni lni-map-marker"></i> Calle española original, 00000, Spain
-                                        </li>
-                                        <li><i class="lni lni-envelope"></i> info@hardwareseller.com</li>
-                                        <li><i class="lni lni-phone"></i> +34-666555111</li>
-                                    </ul>
-                                </div>
-                                <!-- End Single Widget -->
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
+        <div>
+            <img alt="Deployed with FTP Deploy Action" src="https://img.shields.io/badge/Deployed With-FTP DEPLOY ACTION-%3CCOLOR%3E?style=for-the-badge&color=0077b6">
+            <a href=https://github.com/SamKirkland/FTP-Deploy-Action>Házlo tú mismo. Credit @SamKirkland</a>
         </div>
-        <!--/ End Footer Middle -->
-    <!--/ End Footer Area -->   
+</div>
 </footer>
 </body>
 </html>
