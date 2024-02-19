e6f0c261aa7f58ba1c23ad690a3c0c0623d9171f
josepedevs <josepedevs@gmail.com>
2024-02-19 22:46:40 +0100

    ValidarDatosClietne--> alta in situ ahora manda a MetodoDePago.php+ creamos principio del archivo metododepago.php

---
+++ Views/MetodoDePago.php
@@ -0,0 +1,11 @@
+<?php
+if(session_status() !== PHP_SESSION_ACTIVE) {session_start();}
+include_once("header.php");
+?>
+<h1>Seleccione el método de pago</h1>
+
+
+
+<?php
+include_once("header.php");
+?>
