0cbecdb98d7c20f2693469d11a33f7cfea8121db
josepedevs <josepedevs@gmail.com>
2024-02-21 21:54:10 +0100

    DireccionPedido--> mejor debugin para saber que estoy viendo

--- Views/DireccionPedido.php
+++ Views/DireccionPedido.php
@@ -8,16 +8,16 @@ echo'<h1>Dirección de envío</h1>';
 include_once("aside.php");
 
 //RECIBIMOS POR POST LOS DATOS DE CONTENIDOPEDIDO
-
+print"post:<br>";
 print_r($_POST);
 foreach ($_POST as $AtributoYNumero => $valor) {
-// AtributoYNumero que empiecen  por alguna de estas palabras y terminar en un numero
+    // AtributoYNumero que empiecen  por alguna de estas palabras y terminar en un numero
     if (preg_match('/^(codigo|cantidad|precio|descuento)\d+$/', $AtributoYNumero)) {
         $hayNumeros = preg_match('/(\d+)$/', $AtributoYNumero, $matches, PREG_OFFSET_CAPTURE); //mete en $matches si encuentra el regex, es un array multidimensional 
         $posicionNumeros=$matches[0][1];//$matches[0][0] es lo que ha encontrado que coincide con el regex, mientras que $matches[0][1] es donde lo ha encontrado
         print$posicionNumeros;
         if ($hayNumeros) {
-
+            
             $atributo = substr($AtributoYNumero, 0, $posicionNumeros); //coge del principio hasta donde aparece el primer número, eso es el nombre del atributo
             $numLinea = intval(substr($AtributoYNumero, $posicionNumeros)); // en AtributoYNumero buscamos desde donde empiezan los números hasta el final (hacemos 0 offset cuando llegue al final)
             
@@ -26,10 +26,11 @@ foreach ($_POST as $AtributoYNumero => $valor) {
             } else{
                 $productosYCantidadesConfirmadas[$numLinea][$atributo] = $valor;  //metemos dentro de la respectiva numLinea los atributos codigo, descuento, precio y cantidad
             }
-
+            
         }
     }
 }
+print"productos Y cantidades confirmadas:<br>:";
 print_r($productosYCantidadesConfirmadas);
 $_SESSION['productosCarrito'] = $productosYCantidadesConfirmadas; //guardamos los datos del carrito en la sesión para tenerlos a mano
 
