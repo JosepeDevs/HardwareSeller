99cf84c52bf935118f2a78904c581d7c7aafc6d0
josepedevs <josepedevs@gmail.com>
2024-02-21 23:44:05 +0100

    Ficha Articulo--> creada plantilla, usamos controller de buscar articulos, pendiente crear método en modelo de articulo

--- Views/FichaArticulo.php
+++ Views/FichaArticulo.php
@@ -1,14 +1,34 @@
 <?php
+if(session_status() !== PHP_SESSION_ACTIVE) {session_start();}
 include("header.php");
+if(isset($_GET["codigo"])){
+    $codigo = $_GET["codigo"];
+    include_once("../Controllers/ArticuloBUSCARController.php");
+    $articulo = getArticuloByCodigo($codigo);
+} 
+$directorio = "/Resources/ImagenesArticulos/";
+
+echo'<h1>Ficha artículo:'.$articulo->getNombre().'</h1>';
+
+include_once("aside.php");
 ?>
-<h1>Ficha artículo</h1>
-
-
-<?php
-
-
-?>
-
+<section class="imagen-ficha-articulo">
+    <? echo' <a href="FichaArticulo.php?codigo='.$articulo->getCodigo().'">
+                <img src="'.$directorio .$articulo->getImagen().'" class="img-fluid" alt="'.$articulo->getImagen().'">
+            </a>'?>
+</section>
+<section class="Precio-carrito">
+    <div id="precio">
+    <h2>Precio: <? $articulo->getPrecio() ?> €</h2>
+    </div>
+    <div id="carrito">
+        <a href="<? echo '?codigo='.$articulo->getCodigo() ?>">Añadir al carrito  <i class="lni lni-cart-full" alt="Añadir al carrito"></i></a>
+    </div>  
+</section>   
+<section id="descripcion">
+    <p>Descripción del producto:</p><br>
+    <textarea readonly rows=5 cols=40 name="texto" ><?=$articulo->getDescripcion() ?></textarea><br><br>
+</section>
 
 
 <?php
