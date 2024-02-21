a787658cd71ce4428fb711cdeb078248ca6db920
josepedevs <josepedevs@gmail.com>
2024-02-21 21:36:57 +0100

    DireccionPedido--> fallo en el print para hacer debugging

--- Views/DireccionPedido.php
+++ Views/DireccionPedido.php
@@ -15,7 +15,7 @@ foreach ($_POST as $AtributoYNumero => $valor) {
     if (preg_match('/^(codigo|cantidad|precio|descuento)\d+$/', $AtributoYNumero)) {
         $hayNumeros = preg_match('/(\d+)$/', $AtributoYNumero, $matches, PREG_OFFSET_CAPTURE); //mete en $matches si encuentra el regex, es un array multidimensional 
         $posicionNumeros=$matches[0][1];//$matches[0][0] es lo que ha encontrado que coincide con el regex, mientras que $matches[0][1] es donde lo ha encontrado
-        print$posicionNumero;
+        print$posicionNumeros;
         if ($hayNumeros) {
 
             $atributo = substr($AtributoYNumero, 0, $posicionNumeros); //coge del principio hasta donde aparece el primer número, eso es el nombre del atributo
