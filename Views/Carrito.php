7b14433f487fe1be207b6a521d53bf66db1d0d1d
josepedevs <josepedevs@gmail.com>
2024-02-21 20:10:39 +0100

    Carrito ponemos input y e id y nada mas para seleccioinarlo

--- Views/Carrito.php
+++ Views/Carrito.php
@@ -120,7 +120,7 @@ document.addEventListener("DOMContentLoaded", function() {
         var filas= document.querySelectorAll("tbody tr")
         filas.forEach(function(row) {//metemos todas las filas hermanas de todos los tbodys en un array 
        //     var cantidad = parseInt(row.querySelector('input[id^="cantidad"]').value);
-            var cantidad = parseInt(row.querySelector("td div.row span.cantidad input[id^='cantidad']").value);
+       var precio = parseFloat(row.querySelector("input[id^='cantidad']").value); // ^=  es para seleccionar elementos que empiecen por lo que se indique
             console.log("cantidad:");
             console.log(cantidad);
             var precio = parseFloat(row.querySelector("input[id^='precio']").value); // ^=  es para seleccionar elementos que empiecen por lo que se indique
