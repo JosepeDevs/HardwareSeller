9a7346abbdd2ecfe7a1ffb11497c39bb1cf2947a
josepedevs <josepedevs@gmail.com>
2024-02-21 19:48:38 +0100

    Carrito ahora buscamos el id como # y no por name

--- Views/Carrito.php
+++ Views/Carrito.php
@@ -119,7 +119,7 @@ document.addEventListener("DOMContentLoaded", function() {
         // Loop todas las filas
         var filas= document.querySelectorAll("tbody tr")
         filas.forEach(function(row) {//metemos todas las filas hermanas de todos los tbodys en un array 
-            var cantidad = parseInt(row.querySelector("td div span input[id^='cantidad']").value);
+            var cantidad = parseInt(row.getElementById('input[id^="cantidad"]'));
             console.log("cantidad:");
             console.log(cantidad);
             var precio = parseFloat(row.querySelector("input[name^='precio']").value); // ^=  es para seleccionar elementos que empiecen por lo que se indique
