d0ffddd328062e665ae60a534ee24ae04abb640e
josepedevs <josepedevs@gmail.com>
2024-02-22 21:15:24 +0100

    FichaArticulo--> ahora sí se mostrarán relacionados pero no el propio item bien

--- Views/FichaArticulo.php
+++ Views/FichaArticulo.php
@@ -62,7 +62,10 @@ echo'
             }    
             if($arrayArticulosRelacionados !== false){
                 //todo hacerlo carousel y poder meter más de los que caben en pantalla 
-                for($i=0;$i<=6 && $arrayArticulosRelacionados[$i]->getCodigo() !== $codigo;$i++){  //no dejaremos que se muestre el propio item como relacionado
+                for($i=0;$i<=6;$i++){  //no dejaremos que se muestre el propio item como relacionado
+                    if($arrayArticulosRelacionados[$i]->getCodigo() !== $codigo){
+                        continue;
+                    }
                     echo'<td class="col-12 col-lg-1 col-sm-1">';
                     echo'
                     <div>
