cbb276c4263106ac704d46f3f2c2978c727fa8e8
josepedevs <josepedevs@gmail.com>
2024-02-23 20:57:25 +0100

    MetodoDePago--> vamos a esconder parte del formulario hasta que elijan la opción con tarjeta

--- Views/MetodoDePago.php
+++ Views/MetodoDePago.php
@@ -7,18 +7,22 @@ include_once("aside.php");
 ?>
 
 <form action="PedidoALTA.php" method="POST">
-    <select name="estado">
+    <select name="estado" id="estado">
         <option value="3">Transferencia</option>
-        <summary><option value="4">Tarjeta</option></summary>
-            <details>
-                <label for="numeroTarjeta">Número de Tarjeta:</label>
-                <input type="text" id="numeroTarjeta" name="numeroTarjeta" required><br>
-                <label for="caducidadTarjeta">Fecha de Expiración:</label>
-                <input type="month" id="expiracionTarjeta" name="expiracionTarjeta" required><br>
-                <label for="cvv">CVV:</label>
-                <input type="number" id="cvv" name="cvv" required min="100" max="999"><br>
-            </details>
+        <option value="4">Tarjeta</option>
     </select>
+    <div id="detallesTarjeta" style="display: none;">
+        <br>
+        <summary>Detalles de la Tarjeta</summary>
+        <label for="numeroTarjeta">Número de Tarjeta:</label>
+        <br>
+        <input type="text" id="numeroTarjeta" name="numeroTarjeta" required><br>
+        <label for="caducidadTarjeta">Fecha de Expiración:</label>
+        <br>
+        <input type="month" id="expiracionTarjeta" name="expiracionTarjeta" required><br>
+        <label for="cvv">CVV:</label>
+        <input type="number" id="cvv" name="cvv" required min="100" max="999"><br>
+    </div>
     <input type="submit" value="Guardar método de pago y confirmar pedido">
 </form>
 
@@ -26,3 +30,17 @@ include_once("aside.php");
 <?php
 include_once("header.php");
 ?>
+<script>
+document.addEventListener('DOMContentLoaded', function() {
+    var metodoPago = document.getElementById('estado');
+    var detallesTarjeta = document.getElementById('detallesTarjeta');
+
+    metodoPago.addEventListener('change', function() {
+        if (this.value === '4') {
+            detallesTarjeta.style.display = 'block';
+        } else {
+            detallesTarjeta.style.display = 'none';
+        }
+    });
+});
+</script>
