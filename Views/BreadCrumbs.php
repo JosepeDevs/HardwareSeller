5e7a547acc94c36158022d29b1cf43fcbed06d9d
josepedevs <josepedevs@gmail.com>
2024-02-24 23:51:34 +0100

    BreadCrumbs--> le faltaba include para la función de get categoria

--- Views/BreadCrumbs.php
+++ Views/BreadCrumbs.php
@@ -4,6 +4,7 @@ print'
     <li class="breadcrumb-item"><a href="#">Catalogo</a></li>';
     if(isset($_GET["categoria"])) {
         $codigoCategoria = $_GET["categoria"];
+        include_once("../Controllers/CategoriaBUSCARController.php");
         $categoria = getCategoriaByCodigo($codigoCategoria);
         $nombreCategoria = $categoria->getNombre();
         echo'<li class="breadcrumb-item active"><a href="#">'.$nombreCategoria.'</a></li>
