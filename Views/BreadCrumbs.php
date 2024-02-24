043afbc7572a8678ffce154da547fff958264bb7
josepedevs <josepedevs@gmail.com>
2024-02-24 23:55:42 +0100

    BreadCrumbs--> wrappead como div quitamos que sea lista me gusta mas como cosa/cosa/cosa

--- Views/BreadCrumbs.php
+++ Views/BreadCrumbs.php
@@ -1,14 +1,15 @@
 <?php
 print'
-  <ol class="breadcrumb">
-    <li class="breadcrumb-item"><a href="#">Catalogo</a></li>';
+  <div class="breadcrumb">
+    <p class="breadcrumb-item"><a href="/Views/Catalogo.php"> HardWare Seller / </a></p>
+    <p class="breadcrumb-item"><a href="/Views/Catalogo.php"> Catalogo / </a></p>';
     if(isset($_GET["categoria"])) {
         $codigoCategoria = $_GET["categoria"];
         include_once("../Controllers/CategoriaBUSCARController.php");
         $categoria = getCategoriaByCodigo($codigoCategoria);
         $nombreCategoria = $categoria->getNombre();
-        echo'<li class="breadcrumb-item active"><a href="#">'.$nombreCategoria.'</a></li>
+        echo'<p class="breadcrumb-item active"><a href="">'.$nombreCategoria.'</a></p>
         ';
     }
- print' </ol>';
+ print' </div>';
  ?>
