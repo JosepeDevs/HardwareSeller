59005d628b1e3d8e8b7d2076f56edbb46bce598e
josepedevs <josepedevs@gmail.com>
2024-02-24 17:53:48 +0100

    aside--> cambiamos nombre del array en el foreach que estba mal

--- Views/aside.php
+++ Views/aside.php
@@ -3,9 +3,6 @@
             <a href="ArticuloBUSCAR.php">
                 <img class="iconArribaTabla"  src="../Resources/buscaAr.png" alt="recraft icon"/> Buscar artículo
             </a>
-            <a href="Catalogo.php">
-            <img  class="iconArribaTabla" src="../Resources/refresh.png" alt="refrescar" /> Recargar tabla (Quita ordenación y reinicia paginación)
-        </a>
             <a href="/Views/Catalogo.php?Pantallas">Pantallas</a>
             <?php
         //PREPARAR ARRAYS CON CATEGORIAS
@@ -14,7 +11,7 @@
         $arrayCategorias = getArrayCategoriasOrdenados($orden);
 
         //DATOS DE LOS OBJETOS
-        foreach($arrayAImprimir as $categoria) {
+        foreach($arrayCategorias as $categoria) {
             if($categoria->getActivo() == 1){
                 echo '<a href="/Views/Catalogo.php?'.$categoria->getNombre().'">'.$categoria->getNombre().'</a>';
             }
