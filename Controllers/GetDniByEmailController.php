0a870447c3a6b29b8be6283afecaa0abb745c2f5
josepedevs <josepedevs@gmail.com>
2024-02-25 23:36:20 +0100

    getdni desportegido

--- Controllers/GetDniByEmailController.php
+++ Controllers/GetDniByEmailController.php
@@ -1,15 +1,7 @@
 <?php
 if(session_status() !== PHP_SESSION_ACTIVE) {session_start();}
 
-///no proteger no borrar, necesario para clientes nuevos
-include_once("OperacionesSession.php");
-$usuarioLogeado = UserEstablecido();
-if( $usuarioLogeado == false){
-    session_destroy();
-    echo "GetEmailByDniController.php dice: no está user en session";
-    header("Location: ../Views/index.php");
-    exit;
-}
+///no proteger no borrar, necesario para clientes nuevos, necesario también ppara busacr artiuclos
 
 function GetDniByEmail($email){
     $raiz= dirname(__DIR__);
