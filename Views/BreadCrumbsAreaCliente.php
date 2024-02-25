2634cf69f9bce229c50d6a6c4c82b33e7ce0063e
josepedevs <josepedevs@gmail.com>
2024-02-25 18:34:02 +0100

    BreadCrums--> ya se ven bien y añadimos / al final de todos

--- Views/BreadCrumbsAreaCliente.php
+++ Views/BreadCrumbsAreaCliente.php
@@ -7,24 +7,24 @@ echo '
     if(isset($_REQUEST['idPedido']) && !empty($_REQUEST['idPedido']) ){
         //llegamos hasta pedido
         echo'<p><a class="breadcrumb-item " href="PedidosLISTAR.php"> Mis pedidos /</a></p>';
-        echo'<p><a class="breadcrumb-item " href="PedidoBUSCAR.php?idPedido='.$_REQUEST['idPedido'].'">
+        echo'<p><a class="breadcrumb-item " href="PedidoBUSCAR.php?idPedido='.$_REQUEST['idPedido'].' /">
                 Pedido id='.$_REQUEST['idPedido'].' 
             </a></p>
         ';
     } else{
-        echo'<p><a class="breadcrumb-item "  href="PedidosLISTAR.php"> Mis pedidos  </a></p>';
+        echo'<p><a class="breadcrumb-item "  href="PedidosLISTAR.php"> Mis pedidos /</a></p>';
     }
 
     if(isset($_REQUEST['numPedido']) && !empty($_REQUEST['numPedido']) ){
         //entonces estamos ya en contenido pedido
         echo'<p><a class="breadcrumb-item " href="PedidosLISTAR.php"> Mis pedidos /</a></p>';
         echo'
-            <p><a class="breadcrumb-item " href="PedidoBUSCAR.php?idPedido='.$_REQUEST['numPedido'].'">
+            <p><a class="breadcrumb-item " href="PedidoBUSCAR.php?idPedido='.$_REQUEST['numPedido'].' /">
                 Pedido id='.$_REQUEST['numPedido'].' 
             </a></p>
         ';
         echo'
-            <p><a class="breadcrumb-item " href="PedidoBUSCAR.php?numPedido='.$_REQUEST['numPedido'].'">
+            <p><a class="breadcrumb-item " href="PedidoBUSCAR.php?numPedido='.$_REQUEST['numPedido'].' /">
                 Contenido del Pedido id='.$_REQUEST['numPedido'].' 
             </a></p>
         ';
