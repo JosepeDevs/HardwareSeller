31ec7a8d73c8463cae3b4c4aac88f71af647ca8c
josepedevs <josepedevs@gmail.com>
2024-02-25 15:20:21 +0100

    PedidoBorrar--> enlaces mejorados

--- Views/PedidoBORRAR.php
+++ Views/PedidoBORRAR.php
@@ -10,24 +10,24 @@ if( $usuarioLogeado == false){
 
 include_once("header.php");
 
-$numPedido = isset($_GET['numPedido']) ? $_GET['numPedido'] : null;
+$idPedido = isset($_GET['idPedido']) ? $_GET['idPedido'] : null;
 
 
 if(isset($_GET['confirmacion']) && $_GET['confirmacion'] ==  'false' ){
     $_SESSION['BorradoPedidoCancelado'] = true;
     header("Location: PedidoLISTAR.php");
     exit;
-}else if(isset($_GET['numPedido']) && isset($_GET['confirmacion']) && $_GET['confirmacion']== 'true') {
+}else if(isset($_GET['idPedido']) && isset($_GET['confirmacion']) && $_GET['confirmacion']== 'true') {
     include_once("../Controllers/PedidoBORRARController.php");
-    $operacionConfirmada = borradoLogicoPedido($numPedido);
+    $operacionConfirmada = borradoLogicoPedido($idPedido);
     header("Location: PedidoLISTAR.php");
     exit;
 }
 ?>
-        <h1>¿Está seguro de que desea desactivar el contenido de este pedido?</h1>
+        <h1>¿Está seguro de que desea cancelar este pedido y sus contenidos?</h1>
         <div class="finForm">
-            <h2><a href="PedidoBORRAR.php?numPedido=<? echo $numPedido;?>&confirmacion=true">Sí, desactivar la linea del pedido seleccionada (Borrado lógico).</a></h2>
-            <h2><a href="PedidoBORRAR.php?numPedido=<? echo $numPedido;?>&confirmacion=false">Cancelar desactivación.</a></h2>
+            <h2><a href="PedidoBORRAR.php?idPedido=<? echo $idPedido;?>&confirmacion=true">Sí, cancelar pedido y sus contenidos.</a></h2>
+            <h2><a href="PedidoBORRAR.php?idPedido=<? echo $idPedido;?>&confirmacion=false">Mantener pedido.</a></h2>
         </div>
 <?php
 include_once("footer.php");
