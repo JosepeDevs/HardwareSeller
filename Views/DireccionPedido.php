832089e020e81822c83b52870bf677c010b1068f
josepedevs <josepedevs@gmail.com>
2024-02-25 21:21:00 +0100

    DireccionPedido mostrar bien los botones cuando toca

--- Views/DireccionPedido.php
+++ Views/DireccionPedido.php
@@ -110,8 +110,8 @@ unset($_SESSION["productos"]);//nos cargamos la versión simplificada que nos ll
                 if(isset($_SESSION['user'])) {
                     echo"<button type='button'><a href='../Views/MetodoDePago.php?estadoEnvio=5' class='enlace-arriba-de-footer'><i class='lni lni-chevron-right'></i>Proceder al método de pago</a></button>
                     ";
-                    echo"<div class='oculto'>
-                    <button type='button'><a href='../Views/MetodoDePago.php?estadoEnvio=5' id='botonEnvio' class='enlace-arriba-de-footer'><i class='lni lni-chevron-right'></i>Proceder al método de pago>></a></button>
+                    echo"<div id='oculto' style='display: none;'>
+                    <button type='button'><a href='../Views/MetodoDePago.php?estadoEnvio=0' class='enlace-arriba-de-footer'><i class='lni lni-chevron-right'></i>Proceder al método de pago>></a></button>
                     </div>";
                 }else{
                     echo"<input type='submit' value='Proceder al método de pago'/>";
