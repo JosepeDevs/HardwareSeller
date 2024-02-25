0450930a2b3850a72f03d413c1b2b40a355a244e
josepedevs <josepedevs@gmail.com>
2024-02-25 18:29:43 +0100

    BreadCrums--> arregladas

--- Views/BreadCrumbsAreaCliente.php
+++ Views/BreadCrumbsAreaCliente.php
@@ -2,40 +2,33 @@
 echo '
 <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 breadcrumb breadcrumbs">
     <p class="breadcrumb-item"><a href="/index.php"> HardWare Seller / </a></p>
-    <p><a class="breadcrumb-item"  href="AreaCliente.php">Area Personal / </a>
+    <p class="breadcrumb-item"><a href="AreaCliente.php">Area Personal / </a></p>
     ';
     if(isset($_REQUEST['idPedido']) && !empty($_REQUEST['idPedido']) ){
-        echo'<a class="breadcrumb-item " href="PedidosLISTAR.php"> Mis pedidos /</a>
-        </h2></div>';
-        echo'
-            <a class="breadcrumb-item " href="PedidoBUSCAR.php?idPedido='.$_REQUEST['idPedido'].'">
+        //llegamos hasta pedido
+        echo'<p class="breadcrumb-item "><a href="PedidosLISTAR.php"> Mis pedidos /</a></p>';
+        echo'<p class="breadcrumb-item "><a href="PedidoBUSCAR.php?idPedido='.$_REQUEST['idPedido'].'">
                 Pedido id='.$_REQUEST['idPedido'].' 
-            </a></p></div>
+            </a></p>
         ';
     } else{
-        
-        echo'<a class="breadcrumb-item " href="PedidosLISTAR.php"> Mis pedidos  </a>
-        </h2></div>';
+        echo'<p class="breadcrumb-item "><a  href="PedidosLISTAR.php"> Mis pedidos  </a></p>';
     }
 
     if(isset($_REQUEST['numPedido']) && !empty($_REQUEST['numPedido']) ){
-        echo'<a class="breadcrumb-item " href="PedidosLISTAR.php"> Mis pedidos /</a>
-        </h2></div>';
+        //entonces estamos ya en contenido pedido
+        echo'<p class="breadcrumb-item "><a href="PedidosLISTAR.php"> Mis pedidos /</a></p>';
         echo'
-            <a class="breadcrumb-item " href="PedidoBUSCAR.php?idPedido='.$_REQUEST['numPedido'].'">
+            <p class="breadcrumb-item "><a href="PedidoBUSCAR.php?idPedido='.$_REQUEST['numPedido'].'">
                 Pedido id='.$_REQUEST['numPedido'].' 
-            </a></p></div>
+            </a></p>
         ';
-                    echo'
-            <a class="breadcrumb-item " href="PedidoBUSCAR.php?numPedido='.$_REQUEST['numPedido'].'">
+        echo'
+            <p class="breadcrumb-item "><a href="PedidoBUSCAR.php?numPedido='.$_REQUEST['numPedido'].'">
                 Contenido del Pedido id='.$_REQUEST['numPedido'].' 
-            </a></p></div>
+            </a></p>
         ';
-    } else{
-        
-        echo'<a class="breadcrumb-item " href="PedidosLISTAR.php"> Mis pedidos  </a>
-        </h2></div>';
-    }
-
+    } 
+echo'<div>';
 
     ?>
