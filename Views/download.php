2f11ba525f032808b1bbe179f3a3be71f7f15cd4
josepedevs <josepedevs@gmail.com>
2024-02-27 23:50:52 +0100

    cambiamos enlace a ver

--- Views/download.php
+++ Views/download.php
@@ -6,7 +6,7 @@ if (isset($_REQUEST["informe"])) {
 
 //validar con regex el nombre de un archivo
   if (preg_match('/^[^.][-a-z0-9_.]+[a-z]$/i', $informe)) {
-    $rutaArchivo = "Reports/" . $informe;
+    $rutaArchivo = "/Reports/" . $informe;
 
     if (file_exists($rutaArchivo)) {
       header('Content-Description: File Transfer');
@@ -24,7 +24,11 @@ if (isset($_REQUEST["informe"])) {
       die();
     }
   } else {
-    die("Invalid file name!");
+    include_once('header.php');
+    print"<p> Nombre del archivo no válido</p>
+    <a href='InformesLISTAR.php'>Volver al generador de informes</a>";
+    include_once('footer.php');
+    exit;
   }
 }
 
