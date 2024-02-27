d20e5b96a7b0b6712615b4fae7649e5295638b8b
josepedevs <josepedevs@gmail.com>
2024-02-27 22:23:32 +0100

    probamos a cambiar el directorio

--- Controllers/Directorio.php
+++ Controllers/Directorio.php
@@ -10,7 +10,7 @@ if(session_status() !== PHP_SESSION_ACTIVE) {session_start();}
 function PrepararDirectorio() {
     $actualPath = __DIR__;//esto es controllers
     $parentDirectory = dirname($actualPath);//parent directory es algo asi /home/vol8_4/infinityfree.com/if0_35787488/htdocs/
-    $rutaCarpeta =$parentDirectory.'/Reports/ImagenesArticulos/';
+    $rutaCarpeta =$parentDirectory.'/Resources/ImagenesArticulos/';
     if (!file_exists($rutaCarpeta)) {//si no existe el directorio lo crea con permisos totales
         mkdir($rutaCarpeta, 0777, true);
     }
@@ -18,7 +18,9 @@ function PrepararDirectorio() {
 }
 
 function DirectorioInformes() {
-    $rutaCarpeta = 'Reports/';
+    $actualPath = __DIR__;//esto es controllers
+    $parentDirectory = dirname($actualPath);//parent directory es algo asi /home/vol8_4/infinityfree.com/if0_35787488/htdocs/
+    $rutaCarpeta =$parentDirectory.'Reports/';
     if (!file_exists($rutaCarpeta)) {//si no existe el directorio lo crea con permisos totales
         mkdir($rutaCarpeta, 0777, true);
     }
