f0cc8e13ef203e0966bfd4f34dea7cb279c1af10
josepedevs <josepedevs@gmail.com>
2024-02-28 08:00:34 +0100

    no veo el bootstrap

--- Views/InformesLISTAR.php
+++ Views/InformesLISTAR.php
@@ -82,4 +82,6 @@ if($nombreInforme !== false){
 }
 
 include_once("footer.php");
-
+?>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.min.js"></script>
