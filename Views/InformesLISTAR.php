4f2a9f8fa52bf65db66dd665478f8cd8ddd73480
josepedevs <josepedevs@gmail.com>
2024-02-28 08:08:22 +0100

    entramos

--- Views/InformesLISTAR.php
+++ Views/InformesLISTAR.php
@@ -72,6 +72,7 @@ $nombreInforme=false;
 } else if( isset( $_GET["EstadisticasPedidosWeb"] ) && $_GET["EstadisticasPedidosWeb"] == 1 )  {
     $nombreInforme= EstadisticasPedidosWeb($dni);
 } else if( isset( $_POST["FechaInicio"] ) && !empty($_POST['FechaInicio']) && isset($_POST["FechaFin"]) && !empty($_POST['FechaFin']) )  {
+    print"entramos";
     $fechaInicio = !empty($_REQUEST["fechaInicio"]) ? $_REQUEST['fechaInicio'] : null ; 
     $fechaFin = !empty($_REQUEST["fechaFin"]) ?  $_REQUEST['fechaFin'] : null ; 
     $nombreInforme= EstadisticasPedidosRangoFechas($dni,$fechaInicio, $fechaFin);
