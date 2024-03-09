c607de565647ae141b9dab1ddbe021bd2cb1c59e
josepedevs <josepedevs@gmail.com>
2024-03-09 22:58:26 +0100

    a ver que hay en arrayPedido de pedido buscar

--- Views/PedidoBUSCAR.php
+++ Views/PedidoBUSCAR.php
@@ -141,6 +141,7 @@ if( isset($_REQUEST["idPedido"]) || isset($_REQUEST["fechaInicio"]) ||isset($_RE
 
     $arrayAtributos = getArrayAtributosPedido();
     if( $arrayPedido !== false){
+        print_r($arrayPedido);
         echo"<table>";
         echo"<tr>";
         //ENCABEZADOS
