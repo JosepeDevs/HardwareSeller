f1bfc0d963852e5375bff430124dd6bd5055118c
josepedevs <josepedevs@gmail.com>
2024-03-09 18:40:01 +0100

    revisamos session al final del controller

--- Controllers/DireccionPedidoController.php
+++ Controllers/DireccionPedidoController.php
@@ -8,31 +8,32 @@ include_once("header.php");
 if(isset( $_POST['estadoEnvio'] ) && !empty($_POST['estadoEnvio'] )){
     if($_POST['estadoEnvio'] == "tiendaCONcuenta"){
         $_SESSION['estadoEnvio'] = 5;
-        header("Location: ../Views/MetodoDePago.php");
-        exit;
+        //header("Location: ../Views/MetodoDePago.php");
+        //exit;
     } else if($_POST['estadoEnvio'] == "tiendaSINcuenta"){
         $_SESSION['estadoEnvio'] = 5;
         $_SESSION['sinCuenta'] = true;
-        header("Location: ../Views/MetodoDePago.php");
-        exit;
+        //header("Location: ../Views/MetodoDePago.php");
+        //exit;
     }else if($_POST['estadoEnvio'] == "DireccionAreaCliente"){
         $_SESSION['estadoEnvio'] = '0.';
-        header("Location:../Views/DireccionSeleccionada.php");
-        exit;
+        //header("Location:../Views/DireccionSeleccionada.php");
+        //exit;
     }else if($_POST['estadoEnvio'] == "direccionYcuenta"){
         $_SESSION['estadoEnvio'] = '0.';
-        header("Location:../Views/DireccionSeleccionada.php");
-        exit;
+        //header("Location:../Views/DireccionSeleccionada.php");
+        ///exit;
     }else if($_POST['estadoEnvio'] == "direccionSINcuenta"){
         $_SESSION['estadoEnvio'] = '0.';
         $_SESSION['sinCuenta'] = true;
-        header("Location:../Views/DireccionSeleccionada.php");
-        exit;
+        //header("Location:../Views/DireccionSeleccionada.php");
+        //exit;
     } else{
         //en caso de algun error o casos no contemplado avanzaremos con el pedido pero se tendrá que recoger en tienda (si no quieren podrán cancelarlo)
         $_SESSION['estadoEnvio'] = 9999; //un número cualquiera para saber que no ha tenido ningún comportamiento de los esperados
-        header("Location: ../Views/DireccionTiendaSeleccionada.php");
-        exit;
+        //header("Location: ../Views/DireccionTiendaSeleccionada.php");
+       // exit;
     }
+    print_r($_SESSION);
 }
    
