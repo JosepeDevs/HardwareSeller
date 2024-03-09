958a2e2f4f083407d0313dfcf1dd19b099ca9abd
josepedevs <josepedevs@gmail.com>
2024-03-09 20:44:04 +0100

    comentarios en download.php además como no muestra nada lo movemos a controllers y en lugar de mostrar error lo sube a sesison para ser leido en informesListar, añadimos el mensaje a la lista de mensajes de error/confirmación

--- Controllers/download.php
+++ Controllers/download.php
@@ -19,15 +19,15 @@ if (isset($_REQUEST["informe"])) {
     $rutaArchivo = "../Reports/" . $informe;
 
     if (file_exists($rutaArchivo)) {
-      header('Content-Description: File Transfer');
-      header('Content-Type: application/octet-stream');
-      header('Content-Disposition: attachment; filename="' . basename($rutaArchivo) . '"');
-      header('Expires: 0');
-      header('Cache-Control: must-revalidate');
-      header('Pragma: public');
-      header('Content-Length: ' . filesize($rutaArchivo));
-      flush(); // Flush system output buffer
-      readfile($rutaArchivo);
+      header('Content-Description: File Transfer'); // para decirle al navegador que vamos a enviar un archivo
+      header('Content-Type: application/octet-stream'); // Tipo de contenido: flujo binario
+      header('Content-Disposition: attachment; filename="' . basename($rutaArchivo) . '"'); // Contenido como adjunto y el nombre del archivo
+      header('Expires: 0'); // El contenido expira inmediatamente
+      header('Cache-Control: must-revalidate'); // Para asegurar que el contenido se actualice correctamente en el navegador y no salga cache miss o cosas raras.
+      header('Pragma: public'); // Pragma: el informe debe ser accesible  en la red, permitir  almacenamiento en caches públicos.
+      header('Content-Length: ' . filesize($rutaArchivo)); // tamaño del archivo para ver % de descarga (es prácticamente instantaneo)
+      flush(); // Vacia el buffer de salida del sistema. asegura  que  los datos se envien de forma inmediata y no espere a llenarse para mandarlo.
+      readfile($rutaArchivo); // Lee (en este caso envía) el archivo al navegador
       die();
     } else {
       http_response_code(404);
