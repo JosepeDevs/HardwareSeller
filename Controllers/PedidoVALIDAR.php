0724129af2cce941a7832b2c50b409436791e061
josepedevs <josepedevs@gmail.com>
2024-03-09 19:39:03 +0100

    comentamos header en pedidoValidar a ver si es esa línea la que nos lleva a index2

--- Controllers/PedidoVALIDAR.php
+++ Controllers/PedidoVALIDAR.php
@@ -80,7 +80,7 @@ if( isset($_SESSION["editandoPedido"]) && $_SESSION["editandoPedido"] == "true")
     }
     if(isset($_SESSION['CarritoConfirmado']) && !empty($_SESSION['CarritoConfirmado']) ){
         //ahora que ya tenemos el pedido creado y en session vamos a poblar su contenido
-       header("Location: ../Controllers/ContenidoPedidoVALIDAR.php");
+       //header("Location: ../Controllers/ContenidoPedidoVALIDAR.php");
         exit;
     }else{
        // header("Location: ../Views/PedidosLISTAR.php");
