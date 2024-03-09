cdb2ddc38937d3da0a8733c53d365cd00e33e59f
josepedevs <josepedevs@gmail.com>
2024-03-10 00:01:14 +0100

    ficha articulo tamibén muestra ahora la categoria

--- Views/Catalogo.php
+++ Views/Catalogo.php
@@ -91,7 +91,7 @@ echo"<div class='col-lg-9 col-md-11 col-12'>
                             <td>
                                 <div class="row">
                                     <div class="col-12 col-lg-6 col-sm-1">
-                                        <a href="FichaArticulo.php?codigo='.$arrayAImprimir[$i]->getCodigo().'"><img src="'.
+                                        <a href="FichaArticulo.php?categoria='.$arrayAImprimir[$i]->getCategoria().'&codigo='.$arrayAImprimir[$i]->getCodigo().'"><img src="'.
                                         $directorio .$arrayAImprimir[$i]->getImagen().'" class="img-fluid" alt="'.$arrayAImprimir[$i]->getImagen().'"></a>
                                         <br>
                                         <h2>Nombre: '.$arrayAImprimir[$i]->getNombre().'</h2>
