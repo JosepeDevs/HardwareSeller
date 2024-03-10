73855792f12296c0d0c72d376ba68da1e1e3cae5
josepedevs <josepedevs@gmail.com>
2024-03-10 20:35:37 +0100

    prints html tabulador para separar precio de cantidad items2

--- Views/header.php
+++ Views/header.php
@@ -146,7 +146,7 @@
                 if( isset($_SESSION['productos']) && !empty($_SESSION['productos']) ){
                     //contamos cuantos codigos hay en el array de productos 
                     print count($_SESSION['productos']);
-                    print (":&emsp");// imprimirmos un tabulador para separar el número de items del precio total
+                    print (": &ensp");// imprimirmos un tabulador para separar el número de items del precio total
                 }
              ?>
         </small>
