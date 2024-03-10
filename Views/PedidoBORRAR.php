d5273ea2c361c85efecb74b85d1df1584b1e23e4
josepedevs <josepedevs@gmail.com>
2024-03-10 16:01:15 +0100

    vamos a hacer que lo compruebe en session

--- Views/PedidoBORRAR.php
+++ Views/PedidoBORRAR.php
@@ -25,7 +25,7 @@ if(isset($_GET['confirmacion']) && $_GET['confirmacion'] ==  "false" ){
     exit;
 }else if(isset($_GET['idPedido']) && isset($_GET['confirmacion']) && $_GET['confirmacion']== "true" && $estadoCancelable) {
     print($_GET['confirmacion']);
-    if($estadoCancelable == true){
+    if(isset($_SESSION['BadEstadoParaCancelar']) && $_SESSION['BadEstadoParaCancelar'] !== true){
         $operacionExitosa = borradoLogicoPedido($idPedido);
         $_SESSION['PedidoCanceladoExitoso'] = true;
     }
