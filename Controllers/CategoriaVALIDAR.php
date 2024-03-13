3940820558abd7e1281dac81bd04a53a580a8ca0
josepedevs <josepedevs@gmail.com>
2024-03-13 17:14:45 +0100

    codigo nuevo llega por post

--- Controllers/CategoriaVALIDAR.php
+++ Controllers/CategoriaVALIDAR.php
@@ -95,7 +95,7 @@ if( isset($_SESSION["editandoCategoria"]) && $_SESSION["editandoCategoria"] == "
     $activo = ( isset($_SESSION["activo"]) ? $_SESSION["activo"] : 1 );
     $codCategoriaPadre = ( isset($_SESSION["codCategoriaPadre"]) ? $_SESSION["codCategoriaPadre"] : null );//por session llega el código ORIGINAL
 
-    $codigo = ( isset($_SESSION["codigo"]) ? $_SESSION["codigo"] : null ); //por la URL llega el código NUEVO
+    $codigo = ( isset($_POST["codigo"]) ? $_POST["codigo"] : null ); //por la URL llega el código NUEVO
 
     $categoria = new Categoria();
     print($nombre. $codigo. $codigoOriginal. $activo. $codCategoriaPadre);
